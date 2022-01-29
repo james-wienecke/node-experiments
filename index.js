db7c189568ab56898279f687532b4bfb0e0a0aa5
James Wienecke <james.a.wienecke@gmail.com>
2022-01-28 23:43:13 -0600

    Experimenting with the chalk package.

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ import chalk from 'chalk';
 console.log(chalk.blue("this should be blue"));
 console.log(chalk.bgBlackBright("bg black bright"));
 
+let color = {
+    red: 0,
+    green: 0,
+    blue: 0
+};
+
+for (let i = 0; i < 255; i++) {
+    const colorStr = chalk.rgb(color.red, color.green, color.blue);
+    console.log(colorStr("hello"));
+    color.red = (color.red += 32) % 255;
+}
